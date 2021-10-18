913ce1900bb18fb309c17fb93768b92f9a631a5d
chen-98-web <62184954+chen-98-web@users.noreply.github.com>
2021-10-18 15:35:09 +0800

    Update train.py

--- train.py
+++ train.py
@@ -13,3 +13,21 @@ def training(loss, learning_rate):
         global_step = tf.Variable(0, name='global_step', trainable=False)
         train_op = optimizer.minimize(loss, global_step= global_step)
     return train_op
+
+
+def evaluation(logits, labels):
+    """
+     输入
+    logits: 经过cnn_inference处理过的tensor
+    labels:
+    返回
+    accuracy：正确率
+    """
+    with tf.variable_scope('accuracy') as scope:
+        prediction = tf.nn.softmax(logits)  # 这个logits有n_classes列
+        # prediction每行的最大元素（1）的索引和label的值相同则为1 否则为0。
+        correct = tf.nn.in_top_k(prediction, labels, 1)
+        # correct = tf.nn.in_top_k(logits, labels, 1)   也可以不需要prediction过渡，因为最大值的索引没变，这里这样写是为了更好理解
+        correct = tf.cast(correct, tf.float16)  # 记得要转换格式
+        accuracy = tf.reduce_mean(correct)
+    return accuracy
